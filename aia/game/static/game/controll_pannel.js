888a546b97f8dd0ae5310639c7cbb6a265299294
PEPE <ken51006b@hotmail.com>
2020-01-15 11:36:24 +0800

    [WHAT] update controll_pannel.js css

--- aia/game/static/game/controll_pannel.js
+++ aia/game/static/game/controll_pannel.js
@@ -18,11 +18,23 @@ function api_newgame(){
 function btnred(){
     BTN_ON = 1;
     console.log("api_btnred");
+    var ele_red = document.getElementById('btn_red');
+    ele_red.classList.add("disabled");
+
+    var ele_white = document.getElementById('btn_white');
+    ele_white.classList.remove("disabled");
 }
 
 function btnwhite(){
     BTN_ON = 2;
     console.log("api_btnwhite");
+
+    var ele_red = document.getElementById('btn_red');
+    ele_red.classList.remove("disabled");
+
+    var ele_white = document.getElementById('btn_white');
+    ele_white.classList.add("disabled");
+
 }
 
 function api_btnconfirm(){
@@ -42,7 +54,11 @@ function api_btnconfirm(){
     }
     BTN_ON = 0;
     // TODO:
-    // btn clean seleted effect;;
+    var ele_red = document.getElementById('btn_red');
+    ele_red.classList.remove("disabled");
+
+    var ele_white = document.getElementById('btn_white');
+    ele_white.classList.remove("disabled");
 }
 
 function api_btnlock(){
@@ -51,4 +67,3 @@ function api_btnlock(){
     request.send();
     console.log("api_btnlock");
 }
-
